7328c2a4cd5ca521a4d546cb6387cd79f927927e
Pragati Pathak <Pragati.Pathak@vca.com>
2022-08-09 22:27:51 +0530

    Added nullable propertied in JwtSettings model

--- BuberDinner.Infrastructure/Authentication/JwtSettings.cs
+++ BuberDinner.Infrastructure/Authentication/JwtSettings.cs
@@ -2,10 +2,10 @@ namespace BuberDinner.Infrastructure.Authentication
 {
     public class JwtSettings
     {
-        public string Secret { get; set; }
+        public string? Secret { get; set; }
         public int ExpiryMinutes { get; set; }
-        public string Issuer { get; set; }
-        public string Audience { get; set; }
+        public string? Issuer { get; set; }
+        public string? Audience { get; set; }
         
     }
 }
